e6203f7d41970dcf2971188f5754a942e27c6f2e
Andreas Levander <andreas.levander@abo.fi>
2024-09-03 17:43:40 +0200

    added to git

---
+++ python/app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/ping")
+def greet():
+    return jsonify({'message': 'pong'})
+
+#if __name__ == '__main__':
+	#app.run(host='0.0.0.0', port=8090)
